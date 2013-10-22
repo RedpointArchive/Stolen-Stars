a587e9b0ef8cac83ee07a8c76fcbe16de9039194
June Rhodes <jrhodes@redpointgames.com.au>
2013-10-22 18:13:42 +1100

    Add user information when log entries are created.
    Resolves T137.

--- lib/log.php
+++ lib/log.php
@@ -1,11 +1,19 @@
 <?php
 
+final class Log extends DAO {
+  protected $user_id;
+  protected $created;
+  protected $content;
+}
+
 function create_log($db, $content) {
+  global $auth;
   $stmt = $db->prepare("
 INSERT INTO log
-(created, content)
+(user_id, created, content)
 VALUES
-(:created, :content)");
+(:user_id, :created, :content)");
+  $stmt->bindValue(":user_id", $auth->getUser()->getID());
   $stmt->bindValue(":created", time());
   $stmt->bindValue(":content", $content);
   $stmt->execute();
@@ -13,17 +21,24 @@ VALUES
 
 function print_log($db) {
   $stmt = $db->prepare("
-SELECT created, content FROM log
+SELECT created, user.username, log.user_id, content FROM log
+LEFT JOIN user ON user.id = log.user_id
 ORDER BY created DESC");
   $stmt->execute();
   echo '<table style="width: 100%;">
   <tr>
     <th style="text-align: left; width: 300px;">Date</th>
+    <th style="text-align: left;">User</th>
     <th style="text-align: left;">Message</th>
   </tr>';
   while ($row = $stmt->fetch()) {
+    $username = '(not logged)';
+    if ($row['user_id'] !== null) {
+      $username = $row['username'];
+    }
     echo '<tr>
     <td>'.date(DATE_RFC822, $row['created']).'</td>
+    <td>'.$username.'</td>
     <td>'.$row["content"].'</td>
   </tr>';
   }
